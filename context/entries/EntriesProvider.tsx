571ee0bb8baaf8c35979af772a325774b8d119aa
Oswaldo Gomez <oswaldogomezsl@gmail.com>
2023-05-23 01:47:22 -0300

    EntriesProvider reads entries from the database by fetching the '/api/entries' endpoint (just as an example, as this can be done in a more efficient way)

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,8 +1,9 @@
-import { FC, PropsWithChildren, useReducer } from 'react'
+import { FC, PropsWithChildren, useEffect, useReducer } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 import { EntriesContext, entriesReducer } from './'
 import { Entry } from '../../interfaces'
+import { entriesApi } from '../../api'
 
 export interface EntriesState {
   entries: Entry[]
@@ -14,6 +15,14 @@ const Entries_INITIAL_STATE: EntriesState = {
 
 export const EntriesProvider:FC<PropsWithChildren> = ({ children }) => {
   const [state, dispatch] = useReducer(entriesReducer, Entries_INITIAL_STATE)
+  const refreshEntries = async () => {
+    const { data } = await entriesApi.get<Entry[]>('/entries')
+    dispatch({ type: '[Entries] Refresh', payload: data })
+  }
+  useEffect(() => {
+    refreshEntries()
+  }, [])
+
   const addEntry = (description: string) => {
     const payload: Entry = {
       _id: uuidv4(),
--- context/entries/entriesReducer.ts
+++ context/entries/entriesReducer.ts
@@ -2,11 +2,17 @@ import { EntriesState } from './'
 import { Entry } from '../../interfaces'
 
 type EntriesActionType =
+| { type: '[Entries] Refresh', payload: Entry[] }
 | { type: '[Entries] Add-Entry', payload: Entry }
 | { type: '[Entries] Update-Entry', payload: Entry }
 
 export const entriesReducer = (state: EntriesState, action: EntriesActionType): EntriesState => {
   switch (action.type) {
+    case '[Entries] Refresh':
+      return {
+        ...state,
+        entries: [...action.payload]
+      }
     case '[Entries] Add-Entry':
       return {
         ...state,
