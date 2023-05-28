acb8637eda64e54650eba215b1922c4e8e0110c9
Oswaldo Gomez <oswaldogomezsl@gmail.com>
2023-05-27 23:33:30 -0300

    Adds text clarifying that the sidebar menu is demonstrational only

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -21,6 +21,7 @@ export const Sidebar = () => {
       <Box sx={{ width: 250 }}>
         <Box sx={{ padding: '5px 10px' }}>
           <Typography variant='h4'>Menu</Typography>
+          <Typography>(demonstrational only)</Typography>
         </Box>
 
         <List>
